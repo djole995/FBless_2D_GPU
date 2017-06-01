4d496dc9a8540ca7a89c257e6586e55365f3898c
Djole i Beli <milandjokic1995@gmail.com>
2017-06-01 08:35:50 +0200

    Testbench added.

--- pcores/fb_less_2d_gpu_periph_v1_00_a/sim/fb_less_2d_gpu_tb.vhd
+++ pcores/fb_less_2d_gpu_periph_v1_00_a/sim/fb_less_2d_gpu_tb.vhd
@@ -1,46 +1,66 @@
 -- TestBench Template 
 
-  LIBRARY ieee;
-  USE ieee.std_logic_1164.ALL;
-  USE ieee.numeric_std.ALL;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
-  ENTITY testbench IS
-  END testbench;
+library fb_less_2d_gpu_periph_v1_00_a;
+use fb_less_2d_gpu_periph_v1_00_a.all;
 
-  ARCHITECTURE behavior OF testbench IS 
+ENTITY testbench IS
+END testbench;
 
-  -- Component Declaration
-          COMPONENT <component name>
-          PORT(
-                  <port1> : IN std_logic;
-                  <port2> : IN std_logic_vector(3 downto 0);       
-                  <port3> : OUT std_logic_vector(3 downto 0)
-                  );
-          END COMPONENT;
+ARCHITECTURE behavior OF testbench IS 
 
-          SIGNAL <signal1> :  std_logic;
-          SIGNAL <signal2> :  std_logic_vector(3 downto 0);
-          
+      constant DATA_WIDTH           : natural := 32;
+      constant COLOR_WIDTH          : natural := 24;
+      constant ADDR_WIDTH           : natural := 13;
 
-  BEGIN
+		signal clk_i : std_logic := '0';
+      signal rst_n_i        : std_logic := '0';
+		-- RAM
+      signal bus_addr_i     : std_logic_vector(ADDR_WIDTH-1 downto 0) := (others => '0');  -- Address used to point to registers
+      signal bus_data_i     : std_logic_vector(DATA_WIDTH-1 downto 0) := (others => '0');  -- Data to be writed to registers
+      signal bus_we_i       : std_logic := '0';
+		-- VGA --
+		signal pixel_row_i    : unsigned(8 downto 0) := (others => '0');
+		signal pixel_col_i    : unsigned(9 downto 0) := (others => '0');
+		signal phase_i        : unsigned(1 downto 0) := (others => '0');
+		signal rgb_o          : std_logic_vector(COLOR_WIDTH-1 downto 0);
 
+	constant clk_period : time := 10 ns;
+BEGIN
   -- Component Instantiation
-          uut: <component name> PORT MAP(
-                  <port1> => <signal1>,
-                  <port3> => <signal2>
+          uut: entity fb_less_2d_gpu_periph_v1_00_a.fb_less_2d_gpu PORT MAP(
+					clk_i => clk_i,
+					rst_n_i        => rst_n_i,
+					-- RAM
+					bus_addr_i     => bus_addr_i,
+					bus_data_i     => bus_data_i,
+					bus_we_i       => bus_we_i,
+					-- VGA --
+					pixel_row_i    => pixel_row_i,
+					pixel_col_i    => pixel_col_i,
+					phase_i        => phase_i,
+					rgb_o          => rgb_o
           );
 
 
-  --  Test Bench Statements
-     tb : PROCESS
-     BEGIN
+	process
+	begin
+		clk_i <= not clk_i;
+		wait for clk_period/2;
+	end process;
+	
 
-        wait for 100 ns; -- wait until global set/reset completes
 
-        -- Add user defined stimulus here
+	tb : PROCESS
+	BEGIN
+		wait for 3*clk_period;
+		rst_n_i <= '1';
+		
 
-        wait; -- will wait forever
-     END PROCESS tb;
-  --  End Test Bench 
+		wait; -- will wait forever
+	END PROCESS tb;
 
   END;
